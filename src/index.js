6e170ef5b74171b58c42bebffbefa9910f001595
Aline Denizot <aline.denizot2@gmail.com>
2021-04-22 12:18:43 +0200

    first GraphQL request

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
 
-ReactDOM.render(
-  <React.StrictMode>
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache()
+});
+
+
+const GET_LAUNCHES = gql`
+  query Launches {
+    launches (limit: 5){
+        id
+        launch_date_utc
+        launch_success
+        rocket {
+          rocket_name
+        }
+        links {
+          video_link
+        }
+        details
+  }
+}
+`;
+
+function Launches() {
+  const { loading, error, data } = useQuery(GET_LAUNCHES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :( {error.message}</p>;
+
+    console.log(data)
+
+    return data.launches.map((launch) => (
+      <div key={launch.id}>
+      <h2>Lancement du {launch["launch_date_utc"]}</h2>
+      <p>{(launch.launch_success) ? "Réussite": "Echec" }</p>
+      <p>Nom de la fusée : {launch.rocket['rocket_name']}</p>
+      <p><a href="{launch.links['video_link']}">Pour voir ce lancement sur youtube</a></p>
+      <p>Plus de détails : {launch.details}</p>
+      </div >
+    ));
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Exemple de 5 lancements 🚀</h2>
+      <Launches />
+    </div>
+  );
+}
+
+render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
